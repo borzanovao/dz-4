af3ff13b96d7cac050eae36e13ece3f15a8c690e
Olga Borzanova <olechka@MacBook-Air-Olga.local>
2023-04-03 23:07:19 +0200

    two classes added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        SecondClass number1 = new SecondClass();
+        int printedNumber = number1.printNumber();
+            System.out.println(String.format("~~~%d~~~", printedNumber));
+
+        }
+    }
